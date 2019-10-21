5d769dc5560bfa9e3198c92ff79d4d31893fb08f
pedrodiogoneto <pedritoneto@gmail.com>
2019-10-21 17:22:51 +0200

    Dockerize application

--- src/redux/reducers/rootReducer.js
+++ src/redux/reducers/rootReducer.js
@@ -1,5 +1,3 @@
-import {GET_PHONES_LIST, GET_PHONES_LOADING, GET_PHONES_SUCCESS, GET_PHONES_ERROR} from "../actions/actions";
-
 const initialState = { phoneList: [],   loading: false,   error: '' };
 
 export default function reduxSagaReducer(state = initialState, action) {
